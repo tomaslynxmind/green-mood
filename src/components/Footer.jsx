9d92eb692c4c5321f2af11a81a2f3a5243972815
Tomás <tsm130109@gmail.com>
2025-08-14 16:08:35 +0100

    change in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<footer id='footer' className='text-center text-3xl bg-amber-700 h-20 fixed bottom-0 w-full'>
+		<footer id='footer' className=' w-full'>
 			<p className='text-center bg-amber-900 text-white py-4 text-sm'>© {new Date().getFullYear()} Todos os direitos reservados</p>
 		</footer>
 	);
