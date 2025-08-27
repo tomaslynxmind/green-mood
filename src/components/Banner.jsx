f3023482e117f0bbc01c25144301e6e2fccaf10d
Tomás <tsm130109@gmail.com>
2025-08-27 17:10:15 +0100

    removi os botoes de teste

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,12 +8,6 @@ const Banner = () => {
 				</h1>
 				<p>Comer Saudável, é bom para a nossa saúde</p>
 
-				<Button href='#food-menu' className='bg-orange-800 text-white hover:opacity-40 mt-2 '>
-					Veja o nosso Menu
-				</Button>
-				<Button href='#contact' className='bg-green-800 text-white hover:opacity-40 mt-2'>
-					Contactos
-				</Button>
 			</div>
 		</section>
 	);
